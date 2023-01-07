35ebe9e7dbf5b4e53a2713843000056caad35538
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-01-07 14:10:32 +0100

    initial commit

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-);
+app.use(cors());
 // Add Routes
 app.use("/users", require("./routes/auth"));
 app.use("/tasks", require("./routes/todo"));
