bf1a65c25591a8ff11979d8067b88296e891530c
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-01-07 16:14:00 +0100

    initial commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_LINK}`,
+    origin: "https://todo-list-frontend-five.vercel.app",
   })
 );
 // Add Routes
