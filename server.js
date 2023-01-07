a5c7c016474eab5048624bf0a47f9d47c542b897
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-01-07 15:52:46 +0100

    initial commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://fash-fullstack-todolist.netlify.app",
+    origin: `${process.env.FRONTEND_LINK}`,
   })
 );
 // Add Routes
