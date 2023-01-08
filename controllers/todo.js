58e4bd02db24f43ed3dac626639bd2cf4bce8659
Fasina Ayomikun <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-01-08 08:24:05 +0100

    Update todo.js

--- controllers/todo.js
+++ controllers/todo.js
@@ -19,7 +19,9 @@ const createTodo = async (req, res) => {
     if (!title || !deadline) {
       throw new BadRequestError("Please input all needed information.");
     }
-  
+   if (title.length > 50) {
+      throw new BadRequestError("Title cannot be more than 50 characters");
+    }
     const date = new Date(Date.now());
     const milliDeadline = new Date(deadline).getTime();
 
