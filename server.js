b923ae0939e0e73e6b7cfd5f4161d2fe590c4aa4
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-12-30 19:33:22 +0100

    some updated

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.use(
     origin: process.env.FRONTEND_LINK,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_LINK);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 // Add Routes
 app.use("/users", require("./routes/auth"));
 app.use("/tasks", require("./routes/todo"));
