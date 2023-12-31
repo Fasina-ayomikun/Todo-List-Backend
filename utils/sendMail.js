d364458bba65b50267d73938375270b019a7d565
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2023-12-31 17:15:53 +0100

    some updated

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,24 +1,24 @@
 const emailjs = require("@emailjs/nodejs");
 const sendMail = (data) => {
   console.log("sent");
-  emailjs;
-  // .send(
-  //   process.env.EMAILJS_SERVICE_ID,
-  //   process.env.EMAILJS_TEMPLATE_ID,
-  //   data,
-  //   {
-  //     publicKey: process.env.EMAILJS_PUBLIC_KEY,
-  //     privateKey: process.env.EMAILJS_PRIVATE_KEY,
-  //   }
-  // )
-  // .then(
-  //   function (response) {
-  //     console.log("SUCCESS!", response.status, response.text);
-  //   },
-  //   function (err) {
-  //     console.log("FAILED...", err);
-  //   }
-  // );
+  emailjs
+    .send(
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
+      data,
+      {
+        publicKey: process.env.EMAILJS_PUBLIC_KEY,
+        privateKey: process.env.EMAILJS_PRIVATE_KEY,
+      }
+    )
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+      },
+      function (err) {
+        console.log("FAILED...", err);
+      }
+    );
 };
 
 module.exports = sendMail;
